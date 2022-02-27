e2643f0d059ec93fb50f89927b2a3575874d4738
osmanmaltiti <terabyte7778@gmail.com>
2022-02-26 14:59:49 -1200

    version 1.1

--- src/index.js
+++ src/index.js
@@ -5,58 +5,19 @@ const data = {
 };
 const project = [
     {
-        name: 'Zozys',
-        desc: `Zozys is a web app i built for my Father's sawmill company. 
-               It's basically and e-commerce app where clients can buy raw and processed wood, doors, bed frames among others and also place custom orders. 
-               It's soon go into production. Its is responsive and renders good on all devices. 
-               The frontend was built with React, Tailwind and Redux, Node/ExpressJS and JWT for the backend and MongoDB for the database.`,
-        stack: ['ReactJS', 'Redux', 'Tailwind', 'ExpressJS', 'MongoDB', 'JWT'],
-        snapshot: './assets/images/zozys.png',
-        demo: '#',
-        repo: 'https://github.com/osmanmaltiti/project-zero.git',
+        name: 'Zozys'
     },
     {
-        name: 'Quil',
-        desc: `Quil is a twitter inspired web app. 
-               It is the first major app i built with React.
-               I didn't know much about media queries.
-               As a result it does render well on small screens.
-               But fixing that won't be an issue and I intend to make time for that. 
-               The frontend was built with React, Redux and SASS,
-               The backend was built mainly with Express and MongoDB and Firebase for Cloud Storage and Authentication`,
-        stack: ['React', 'Redux', 'SASS', 'ExpressJS', 'MongoDB', 'Firebase'],
-        snapshot: './assets/images/quil.png',
-        demo: 'https://quil-overhaul.netlify.app',
-        repo: 'https://github.com/osmanmaltiti/project-one.git',
+        name: 'Quil'
     },
     {
-        name: 'ImageSlider',
-        desc: `This is a mini image carousel I built with React and Tailwind. It is a responsive component that can be used in any project. HTML version of this Component is also available in my repos`,
-        stack: ['ReactJS', 'Tailwind'],
-        snapshot: './assets/images/imageSlider.png',
-        demo: '#',
-        repo: 'https://github.com/osmanmaltiti/react-image-slider.git',
+        name: 'ImageSlider'
     },
     {
-        name: 'IotControl',
-        desc: `This android app was built to contol and monitor the interior conditions of an egg incubator.
-               The app interacted with a nodeMCU which read instructions and fed data to the firebase RTDB. 
-               It was built with Flutter as the frontend and Firebase as the backend.`,
-        stack: ['Flutter', 'Firebase'],
-        snapshot: './assets/images/iotControl.png',
-        demo: '#',
-        repo: 'https://github.com/osmanmaltiti/iotcontrol_flutterApp.git',
+        name: 'IotControl'
     },
     {
-        name: 'Malfolio',
-        desc: `This is the first version of my portfolio app. 
-               It was built with GastbyJS and styled with the SASS preprocessor and finally hosted on Gatsby Cloud.
-               It is responsive and renders decently on all devices.
-               For this current version, I decided to develop the portfolio app without any javascript libraries.`,
-        stack: ['GatsbyJS', 'SASS'],
-        snapshot: './assets/images/portfolio.png',
-        demo: 'https://malfolio.gatsby.io',
-        repo: 'https://github.com/osmanmaltiti/project_portfolio.git',
+        name: 'Malfolio'
     }
 ];
 let state = false;
@@ -77,7 +38,6 @@ const getHTML = {
             getHTML.email.value = '';
             getHTML.message.value = '';
         }),
-    projects: document.getElementById('project-map'),
     openResume: document.getElementById('open').addEventListener('click', () => {
         let open = document.getElementById('resume');
         open.setAttribute('class', 'fixed top-0 w-screen h-screen flex flex-col justify-center')
@@ -96,7 +56,7 @@ const getHTML = {
             elements.forEach(item => {
                 if(item === 'main' || item === 'form'){
                     document.getElementById(item)
-                        .setAttribute('style', 'color: black; background-color: #d7d7d7; font-weight: bold');
+                        .setAttribute('style', 'color: black; background-color: #d7d7d7;');
                 } else {
                     document.getElementById(item)
                         .setAttribute('style', 'background-color: #b1b1b1');
@@ -131,32 +91,3 @@ const getHTML = {
     })
 };
 
-const { projects } = getHTML;
-
-
-projects.innerHTML = project.map(item => 
-            `<div id=${item.name} class=" bg-medium p-4 flex flex-col md:grid md:grid-cols-2 w-full md:justify-self-center md:place-self-center md:rounded-md">
-                <span class="w-full">
-                    <h2 class="text-xl font-semibold text-red-500">${item.name}</h2>
-                    <h3 class="my-2 text-base text-black w-fit px-2 bg-red-200 font-bold">Description</h3>
-                    <p>${item.desc}</p>
-                    <h3 class="my-2 text-base text-black w-fit px-2 bg-red-300 font-bold">Stack used</h3>
-                    <ul class="grid grid-cols-4 md:flex w-full md:flex-row  gap-2">
-                        ${ item.stack.map(item => `<li>${item}</li>`).join(' ') }
-                    </ul>
-                    <span class="flex flex-row w-full justify-evenly md:justify-start md:gap-4 my-4">
-                        <a href=${item.demo} id='button' class="bg-red-600 shadow-light shadow py-2 font-bold px-4 rounded">Live Demo</a>
-                        <a href=${item.repo} id='button' class="bg-black py-2 px-4 shadow-light shadow font-bold rounded">Go To Repo</a>
-                    </span>
-                </span>
-            </div>`).join(' ')
-
-project.forEach((item) => {
-    let img = new Image();
-    let url = item.snapshot;
-    let container = document.getElementById(item.name);
-    
-    img.onload = () => container.appendChild(img);
-    img.src = url
-    img.setAttribute('class', 'md:w-1/2 rounded-lg md:justify-self-end md:place-self-center shadow')
-});
